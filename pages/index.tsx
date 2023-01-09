17ec04d54fde42ac43fa044db0675eb52448d208
Esther Mureithi <estherdee27@gmail.com>
2023-01-09 11:59:36 +0300

    install next-auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -102,20 +102,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+         
         </div>
       </main>
     </>
